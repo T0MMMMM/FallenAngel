a6178e0bdc966d99390d7998ae38363f30357187
T0MMMMM <tf.34500@gmail.com>
2023-12-13 12:52:07 +0100

    dynamicJump

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,22 +5,33 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody rb;
+    public GameObject model;
+
+    // Ground //
     private bool isGrounded = false;
+    public Vector3 boxSize;
+    public float maxDistance;
+    public LayerMask layerMask;
+    // Ground End //
 
     // Double Jump //
     private int doubleJump = 0;
     private int maxJumpNumber = 0;
+    public float jumpForce = 20f;
+    private float jumpTimeCounter;
+    public float jumpTime = 0.35f;
+    private bool isJumping;
     // Double Jump End //
 
     // Dash //
     private bool canDash = false;
     private float dashTime = 0.4f;
-    private float dashPower = 1500f;
+    private float dashPower = 500f;
     //private float dashCooldown = 0.75f;
     // Dash End //
 
     // Character //
-    [SerializeField] private float movementSpeed = 20f;
+    public float movementSpeed = 20f;
     float moveDir = 1f;
     // Character End //
 
@@ -36,39 +47,66 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
 
+        isGrounded = Physics.BoxCast(transform.position, boxSize, -transform.up, transform.rotation, maxDistance, layerMask);
+
         if (isGrounded) {
             doubleJump = maxJumpNumber;
         }
 
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        float verticalInput = Input.GetAxisRaw("Vertical");
 
         rb.velocity = new Vector3(horizontalInput * movementSpeed, rb.velocity.y, verticalInput * movementSpeed);
 
         if (horizontalInput > 0) {
-            moveDir = 1;
-        } else if (horizontalInput < 0) {
-            moveDir = -1;
+            if (canDash) { moveDir = -1; }
+            model.transform.eulerAngles = new Vector3(0, 180, 0);
+        } else if (horizontalInput < 0 && canDash) {
+            if (canDash) { moveDir = -1; }
+            model.transform.eulerAngles = new Vector3(0, 0, 0);
         }
 
-        if (Input.GetButtonDown("Jump"))
+        // JUMP //
+        if (Input.GetKeyDown(KeyCode.Space) && (isGrounded || doubleJump >= 1 ))
         {
-            if (isGrounded)
-            {
-                rb.AddForce(new Vector3(0, 7, 0), ForceMode.Impulse);
-            } else if (doubleJump >= 1) {
-                doubleJump -= 1;
-                rb.AddForce(new Vector3(0, 10, 0), ForceMode.Impulse);
-            }
-            
+            rb.velocity = Vector3.up * jumpForce;
+            isJumping = true;
+            jumpTimeCounter = jumpTime;
+            doubleJump -= 1;
         }
-        
+
+        if (Input.GetKey(KeyCode.Space) && isJumping)
+        {
+            if (jumpTimeCounter > 0)
+            {
+                rb.velocity = Vector3.up * jumpForce;
+                jumpTimeCounter -= Time.deltaTime;
+            } else {
+                isJumping = false;
+            }
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            isJumping = false;
+        }
+        // JUMP END //
+
+        // DASH //
         if (canDash && Input.GetButtonDown("Dash"))
         {
             StartCoroutine(Dash());
         }
+        // DASH END //
 
     }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color=Color.red;
+        Gizmos.DrawCube(transform.position-transform.up*maxDistance, boxSize);
+    }
+
+
     private IEnumerator Dash() {
         canDash = false;
         float startTime = Time.time;
@@ -80,23 +118,6 @@ public class PlayerMovement : MonoBehaviour
             yield return null;
         }
         canDash = true;
-    }
-
-
-    private void OnCollisionEnter(Collision other)
-    {
-        if (other.rigidbody == null)
-        {
-            isGrounded = true;
-        }
-    }
-
-    private void OnCollisionExit(Collision other)
-    {
-        if (other.rigidbody == null)
-        {
-            isGrounded = false;
-        }
     }
 
     private void OnTriggerEnter(Collider other)
